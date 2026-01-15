81a8756bac392134219f1e16e2dfa13716abd754
carlosaltan18 <carlosaltan850@gmail.com>
2026-01-14 20:58:10 -0600

    Proyecto Base (Mateo 16:18 Y yo también te digo, que tú eres Pedro, y sobre esta roca edificaré mi iglesia;)

---
+++ src/main/Main.java
@@ -0,0 +1,9 @@
+package main;
+/**
+ * Main class to run the Radio application.
+ * @author Grupo 17
+ * 
+ */
+public class Main {
+    
+}
---
+++ src/model/RadioCarToyota.java
@@ -0,0 +1,9 @@
+package model;
+/**
+ * Class implementing a Radio Interface for Toyota cars.
+ * @author Grupo 17
+ * 
+ */
+public class RadioCarToyota {
+    
+}
